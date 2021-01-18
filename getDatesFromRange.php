7eeec1f226d74dd07befc5020080e870f6bc4e70
aqshalzakki <46075252+aqshalzakki@users.noreply.github.com>
2021-01-18 15:25:23 +0700

    Create getDatesFromRange.php

---
+++ getDatesFromRange.php
@@ -0,0 +1,21 @@
+// Function to get all the dates in given range
+  public static function getDatesFromRange($start, $end = null, $format = 'd-m-Y'): array
+  {
+    $end ??= date($format);
+
+    // Variable that store the date interval 
+    // of period 1 day 
+    $interval = new \DateInterval('P1D');
+
+    $end = new \DateTime($end);
+    $end->add($interval);
+
+    $period = new \DatePeriod(new \DateTime($start), $interval, $end);
+
+    $dates = [];
+    foreach ($period as $date) {
+      $dates[] = $date->format($format);
+    }
+
+    return $dates;
+  }
